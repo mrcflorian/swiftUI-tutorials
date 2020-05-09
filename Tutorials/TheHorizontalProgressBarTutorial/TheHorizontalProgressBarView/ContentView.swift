b4ceac86a62c7649876e640aaed055815fd4e1b7
iOS Dev 1412 <48834551+iOSDev1412@users.noreply.github.com>
2020-05-09 13:01:40 +0700

    Add files via upload
    Upload the Horizontal Progress Bar View

---
+++ Tutorials/TheHorizontalProgressBarTutorial/TheHorizontalProgressBarView/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  TheHorizontalProgressBarView
+//
+//  Created by iOS App Templates on 5/9/20.
+//  Copyright © 2020 iOS App Templates. All rights reserved.
+//
+
+import SwiftUI
+
+struct HorizontalProgressBarView: View {
+    @Binding var currentProgressValue: CGFloat
+    let width: CGFloat
+    let height: CGFloat
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.gray)
+            Rectangle()
+                .fill(Color.purple)
+                .frame(width: min(width, currentProgressValue*width))
+        }.cornerRadius(30.0)
+            .frame(width: width, height: height)
+    }
+}
+
+struct ContentView: View {
+    @State var currentProgressValue: CGFloat = 0.0
+    
+    var body: some View {
+        HorizontalProgressBarView(currentProgressValue: $currentProgressValue,
+                        width: 300,
+                        height: 10)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
